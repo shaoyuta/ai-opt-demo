07ad425e350d98b6a5fbad923b452ed6d0e09aa7
taosy <shaoyu.tao@intel.com>
2023-04-09 21:52:57 +0800

    update files

--- resnet50/chap04.py
+++ resnet50/chap04.py
@@ -1,6 +1,6 @@
 '''
-cuda is ok
-python3 chap03.py    --num_warmup 10 --num_iter 50
+basic
+python3 chap04.py    --num_warmup 10 --num_iter 50
 '''
 import argparse
 import time
@@ -10,8 +10,6 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-#parser.add_argument('--gpu', default=None, type=int,
-#                    help='GPU id to use.')
 parser.add_argument('--num_iter', type=int, default=-1, help='num_iter')
 parser.add_argument('--num_warmup', type=int, default=-1, help='num_warmup')
 
@@ -25,20 +23,8 @@ def main_worker(args):
     print("=> using pre-trained model '{}'".format("resnet50"))
     model = models.__dict__["resnet50"](pretrained=True)
 
-    if not torch.cuda.is_available() :
-        print('using CPU, this will be slow')
-    else:
-        # DataParallel will divide and allocate batch_size to all available GPUs
-        model = torch.nn.DataParallel(model).cuda()
+    device = torch.device("cpu")
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-  
-   
-    # Data loading code
-    print("=> Dummy data is used!")
     val_dataset = datasets.FakeData(50000, (3, 224, 224), 1000, transforms.ToTensor())
 
     val_sampler = None
@@ -61,12 +47,10 @@ def validate(val_loader, model, args):
 
                 # input to device
                 elapsed = time.time()
-                if torch.cuda.is_available():
-                    target = target.cuda(non_blocking=True)
 
                 # compute output
                 model(images)
-                if torch.cuda.is_available(): torch.cuda.synchronize()
+
                 elapsed = time.time() - elapsed
                 print("Iteration: {}, inference time: {} sec.".format(i, elapsed), flush=True)
                 if i >= args.num_warmup:
