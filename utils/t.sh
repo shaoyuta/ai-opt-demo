09ebf1bf723790064e0643599ed9999c67864c7a
taosy <shaoyu.tao@intel.com>
2023-11-08 08:05:14 -0500

    update

--- utils/t.sh
+++ utils/t.sh
@@ -2,37 +2,25 @@
 
 set -e
 
-cmd='/home/taosy/llm/1-c4709ac180e/workspace/run_test.sh'
-str_roi='Iteration:'
-
-
-function foo(){
-    for i in {1..5};do
-        echo ${i}:
-        sleep 1
-    done
-}
-
-#( eval ${cmd} > backlight ) &
-#exec 3< backlight
-
+cmd='/home/taosy/llama2/1-c4709ac180e/workspace/run_test.sh'
+str_roi_start='Iteration'
+str_roi_end='Summary'
+trace_started=false
 
 exec 3< <(eval ${cmd})
-#str_roi='3:'
-#exec 3< <(foo)
 
 while read <&3 line; 
 do 
-    echo ===${line}
-    if [[ $(echo ${line} | grep ${str_roi}) ]]; then
-        echo -n +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    echo : ${line}
+    if [[ ${trace_started} = false ]] && [[ $(echo ${line} | grep ${str_roi_start}) ]]; then
         emon -collect-edp -f emon.dat &
-        break
+        trace_started=true
+    fi
+    if [[ ${trace_started} = true ]] && [[ $(echo ${line} | grep ${str_roi_end}) ]]; then
+        emon -stop
+        trace_started=false
     fi
 done
 
-
-
 wait 
-echo "finished"
-emon -stop
+echo "::: finished"
