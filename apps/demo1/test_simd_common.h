7f69eb4887f9d8d1374c661a8eb6e469683d0480
taosy <shaoyu.tao@intel.com>
2023-11-11 12:17:46 -0500

    update files

---
+++ apps/demo1/test_simd_common.h
@@ -0,0 +1,8 @@
+#ifndef _TEST_SIMD_COMMON_H_
+#define _TEST_SIMD_COMMON_H_
+
+void test_x86_sse_fp32(void);
+void test_x86_sse_fp64(void);
+
+
+#endif
---
+++ apps/demo1/test_sse.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <x86intrin.h>
+#include "tc.h"
+#include "test_simd_common.h"
+
+void test_simd_sse_main(void) {
+    test_x86_sse_fp32();
+}
+
+TestCase simd_sse={
+  .tc_name = "simd_sse",
+  .run=[](TestCase* pcase){
+    test_simd_sse_main();
+    return;
+  },
+};
+REGISTER_TC(simd_sse);
